a729f096777c9b3ee982fe44e6d4261985d29888
QKIvan <ivan33@163.com>
2024-12-31 13:35:49 +0800

    audio: Drop dolby prop override
    * Already defined in device tree
    
    Change-Id: If1bba2745ea234a32d80a94697566d43d133bd80

--- configs/msm8909/msm8909.mk
+++ configs/msm8909/msm8909.mk
@@ -161,9 +161,6 @@ vendor.audio.sys.init=false
 
 #Enable DS2 feature for Dolby
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.audio.dolby.ds2.enabled=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.audio.hw.binder.size_kbyte=1024
 
 #Disable split a2dp
